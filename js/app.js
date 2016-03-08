49750bdbf0cf5c8a10e96367e620d7a730b1aab8
rhonaldomaster <rhonaldomaster@gmail.com>
2016-03-08 14:47:57 -0500

    Funcionalidades js
    Funcionalidades js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-//window.localStorage.setItem("contacts","1;Maria;@galymaster;+573103000000;Anywhere;maria@mail.com");
+//window.localStorage.setItem("contacts","1;Maria;@mariagal;maria@mail.com";+573103000000;Anywhere;0);
 //window.localStorage.removeItem("contacts");
-//window.localStorage.setItem("favorites","1");
-//window.localStorage.removeItem("favorites");
+//window.localStorage.setItem("favs","1");
+//console.log(window.localStorage.getItem("favs"));
+//window.localStorage.removeItem("favs");
 $(document).ready(function(){
     adrbook.listContacts();
     adrbook.listFavorites();
 });
 var adrbook = (function() {
     var dmodal = $(".bgmodal");
-    var datousr = {id:0,nombre:"",twitter:"",email:"",telefono:"",direccion:"",funcion:"createContact()",modify:false,startype:"textlgray"};
+    var datousr = {
+        id:0,nombre:"",twitter:"",email:"",telefono:"",
+        direccion:"",funcion:"createContact()",modify:false,startype:"textlgray"
+    };
+    var listcontacts = "";
+    var listfavs = "";
     var scontact = function(id) {
-        var cnt = {id:0,nombre:"",twitter:"",email:"",telefono:"",direccion:"",funcion:"createContact()",modify:false,startype:"textlgray"};
-        var listcontacts = window.localStorage.getItem("contacts");
+        var cnt = datousr;
         if(listcontacts){
             var lst = listcontacts.split("|");
             for(i=0;i<lst.length;i++){
@@ -24,7 +29,7 @@ var adrbook = (function() {
 					cnt.email = elem[3];
 					cnt.telefono = elem[4];
 					cnt.direccion = elem[5];
-					cnt.funcion = "editContact("+elem[0]+")";
+					cnt.funcion = "updateContact("+elem[0]+")";
                     cnt.modify = true;
                     cnt.startype = isfavo?"textyellow":"textlgray";
 				}
@@ -46,27 +51,107 @@ var adrbook = (function() {
     var cmodal = function() {
         dmodal.hide();
     };
-    var afav = function(id){
-        var favs = window.localStorage.getItem("favorites");
-        favs += "|"+id;
-        window.localStorage.removeItem("favorites");
-        window.localStorage.setItem("favorites",favs);
-    };
-    var isfv = function(id) {
-        var isfavorite = false;
-        var favs = window.localStorage.getItem("favorites");
-        var lst = favs.split("|");
-        for(i=0;i<lst.length;i++){
-            if(id==lst[i]) isfavorite = true;
+    var afav = function(id,elem){
+        var flist = "";
+        var nuevo = false;
+        var element = $(elem);
+        if(element.hasClass("textyellow")){
+            element.removeClass("textyellow");
+            element.addClass("textlgray");
+            var afavs = favs.split("|");
+			$.each(afavs,function(i2,v){
+				if(id!=v){
+					if(i2>0) flist += "|";
+					flist += v;
+				}
+			});
+            if(flist=="") window.localStorage.removeItem("favs");
+            else {
+                window.localStorage.setItem("favs",flist);
+            }
         }
-        return isfavorite;
-    }
+        else{
+            if(!listfavs){
+                listfavs = id;
+                nuevo = true;
+            }
+            else listfavs += "|"+id;
+            window.localStorage.setItem("favs",listfavs);
+            element.addClass("textyellow");
+            var divcnt = $(".maincontent__favorites .contact-list");
+            var ctmpl = $("#contact-template");
+            if(nuevo) divcnt.html("");
+            var source = ctmpl.html();
+            var template = Handlebars.compile(source);
+            var cnt = scontact(id);
+            divcnt.append(template(cnt));
+            alert("Contacto agregado a favoritos!");
+        }
+    };
+    var isfv = function(id){
+        var isf = false;
+        var listfvs = listfavs;
+        if(listfvs){
+            var lst = (listfvs+"").split("|");
+            for(i=0;i<lst.length;i++){
+                if(id==lst[i]) isf = true;
+            }
+        }
+        return isf;
+    };
     var ccontact = function() {
         var nxtid = cantcontacts() + 1;
+        var lc = "";
+        var nombre = $.trim($("#nombre").val());
+        var twitter = $.trim($("#twitter").val());
+        var email = $.trim($("#email").val());
+        var telefono = $.trim($("#telefono").val());
+        var direccion = $.trim($("#direccion").val());
+        if(listcontacts!="" && listcontacts!=null){
+            lc = listcontacts;
+            lc += "|";
+        }
+        lc += nxtid+";"+nombre+";"+twitter+";"+email+";"+telefono+";"+direccion;
+        listcontacts = lc;
+        window.localStorage.setItem("contacts",lc);
+        alert("Contacto agregado!");
+        cmodal();
+        var cnt = {
+            id:nxtid,nombre:nombre,twitter:twitter,
+            email:email,telefono:telefono,direccion:direccion,
+            funcion:"editContact("+nxtid+")",modify:true,startype:"textlgray"
+        };
+        var divcnt = $(".maincontent__contacts .contact-list");
+        var ctmpl = $("#contact-template");
+        if(cnt.id==1) divcnt.html("");
+        var source = ctmpl.html();
+        var template = Handlebars.compile(source);
+        divcnt.append(template(cnt));
+    };
+    var econtact = function(id) {
+        var nombre = $.trim($("#nombre").val());
+        var twitter = $.trim($("#twitter").val());
+        var email = $.trim($("#email").val());
+        var telefono = $.trim($("#telefono").val());
+        var direccion = $.trim($("#direccion").val());
+        var alcont = listcontacts.split("|");
+        var flist = "";
+        $.each(alcont,function(i2,v){
+            var vx = v.split(";");
+            if(i2>0) flist += "|";
+            if(id!=vx[0]) flist += v;
+            else flist += id+";"+nombre+";"+twitter+";"+email+";"+telefono+";"+direccion;
+        });
+        if(flist=="") window.localStorage.removeItem("contacts");
+        else {
+            listcontacts = flist;
+            window.localStorage.setItem("contacts",flist);
+        }
+        alert("Datos actualizados");
+        cmodal();
     };
     var cantcontacts = function() {
         var cant = 0;
-        var listcontacts = window.localStorage.getItem("contacts");
         if(listcontacts){
             var lst = listcontacts.split("|");
             cant = lst.length;
@@ -78,9 +163,11 @@ var adrbook = (function() {
         var ctmpl = $("#contact-template");
         divcnt.html("");
         var cnt = {id:0,nombre:"",twitter:"",email:"",telefono:"",direccion:"",funcion:"createContact()",modify:false,startype:"textlgray"};
-        var listcontacts = window.localStorage.getItem("contacts");
+        listcontacts = window.localStorage.getItem("contacts");
         if(listcontacts){
             var lst = listcontacts.split("|");
+            var source = ctmpl.html();
+            var template = Handlebars.compile(source);
             for(i=0;i<lst.length;i++){
                 var elem = lst[i].split(";");
                 var isfavo = isfv(elem[0]);
@@ -93,8 +180,6 @@ var adrbook = (function() {
 				cnt.funcion = "editContact("+elem[0]+")";
                 cnt.modify = true;
                 cnt.startype = isfavo?"textyellow":"textlgray";
-                var source = ctmpl.html();
-                var template = Handlebars.compile(source);
                 divcnt.append(template(cnt));
 			}
         }
@@ -106,30 +191,31 @@ var adrbook = (function() {
     };
     var lfavs = function() {
         var divcnt = $(".maincontent__favorites .contact-list");
-        var listcontacts = window.localStorage.getItem("favorites");
         var ctmpl = $("#contact-template");
         divcnt.html("");
-        if (listcontacts) {
-            var lst = listcontacts.split("|");
+        listfavs = window.localStorage.getItem("favs");
+        if(listfavs) {
+            var lst = listfavs.split("|");
+            var source = ctmpl.html();
+            var template = Handlebars.compile(source);
             for(i=0;i<lst.length;i++){
                 var cnt = scontact(lst[i]);
-                var source = ctmpl.html();
-                var template = Handlebars.compile(source);
                 divcnt.append(template(cnt));
 			}
         }
-        else {
+        else{
             var source = $("#nocontacts-template").html();
             var template = Handlebars.compile(source);
             divcnt.html(template());
         }
     };
     return{
-        fav: afav,
+        addFavorite: afav,
         editContact: smodal,
         openModal: omodal,
         closeModal: cmodal,
         createContact: ccontact,
+        updateContact: econtact,
         listContacts: lcontacts,
         listFavorites: lfavs
     };
